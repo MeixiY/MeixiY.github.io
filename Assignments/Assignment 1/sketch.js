2fbc039e0141c78a5448c8350cda742fe7de15c1
MeixiY <ymeixi2008@gmail.com>
2026-03-03 13:04:55 -0600

    commit

--- Assignments/Assignment 1/sketch.js
+++ Assignments/Assignment 1/sketch.js
@@ -5,12 +5,15 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+
+// Global Variable Section
 let currentX = 50;
 let currentY = 120;
 let x2 = 500;
 let y2 = 120;
 let circleD = 100;
 let backNum = 0;
+document.onmousedown = click;
 
 
 function setup() {
@@ -20,6 +23,8 @@ function setup() {
   
 }
 
+
+// Scene #1 - Daytime
 function dayScene() {
   noStroke()
   fill("green");
@@ -28,12 +33,25 @@ function dayScene() {
   rect(0, 0, width, (height / 3) * 2);
   fill("yellow");
   circle((width / 8) * 7, (height / 8), 150);
-  hillSheep();
-  clouds(x2, y2);
+  noStroke();
+  fill("green");
+  ellipse(width / 8, (height / 5) * 4, 300, 600);
+  stroke("darkgreen");
+  line(0, (height / 3) * 2, width, (height / 3) * 2);
+  clouds(x2, y2);                // Calling function to draw clouds
   clouds(currentX, currentY);
-
 }
 
+// Clouds added to the sky
+function clouds(x, y) {
+  noStroke();
+  fill("white");
+  circle(x, y, 50);
+  circle(x + 40, y - 10, 70);
+  circle(x + 80, y - 10, 50);
+}
+
+// Move the clouds by dragging
 function mouseDragged() {
   if (dist(currentX, currentY, mouseX, mouseY) < 100) {
     currentX = mouseX;
@@ -44,10 +62,11 @@ function mouseDragged() {
     x2 = mouseX;
     y2 = mouseY;
   }
-
-
 }
 
+
+
+//Scene #2 - Nighttime
 function nightScene() {
   noStroke()
   fill("darkgreen");
@@ -56,60 +75,95 @@ function nightScene() {
   rect(0, 0, width, (height / 3) * 2);
   fill("Moccasin");
   circle((width / 8) * 7, (height / 8), 150);
+  stars(); // Called function for drawing stars
 }
 
-function hillSheep() {
-  noStroke();
-  fill("green");
-  ellipse(width / 8, (height / 5) * 4, 300, 600);
-  stroke("darkgreen");
-  line(0, (height / 3) * 2, width, (height / 3) * 2);
+// Add random stars to the sky
+function stars(){
+  randomSeed(0);
+  for(i = 0; i <= 25; i++){
+    let x = random(width);
+    let y = random(height/2);
+    noStroke();
+    fill(255);
+    quad(x, y, x+2, y+4, x, y+8, x-2, y+4);
+  }
+}
 
+
+
+// Scene #3 - Desert
+function desert(){
+noStroke()
+fill("Goldenrod");
+rect(0, (height / 3) * 2, width, height / 3);
+fill("Khaki");
+rect(0, 0, width, (height / 3) * 2);
+fill(255, 140, 0);
+circle((width / 8) * 7, (height / 8), 150);
+pyramid();
+}
+
+// Draws the two pyramids
+function pyramid(){
+  fill("peru");
+  stroke(139, 69, 19);
+  triangle(0, (height/3)*2, 400, (height/3)*2, 200, (height/5)*2);
+  triangle(200, (height/3)*2, 500, (height/3)*2, 350, (height/4)*2);
+}
+
+
+
+// Scene #4 - At Sea
+function atSea(){
+  noStroke()
+  fill("darkblue");
+  rect(0, (height / 3) * 2, width, height / 3);
+  fill("skyblue");
+  rect(0, 0, width, (height / 3) * 2);
+  fill("yellow");
+  circle((width / 8) * 7, (height / 8), 150);
 
 }
 
-function clouds(x, y) {
-  noStroke();
-  fill("white");
-  circle(x, y, 50);
-  circle(x + 40, y - 10, 70);
-  circle(x + 80, y - 10, 50);
-}
 
+
+// Change colour of ghost bot depending on where the mouse is
 function botColor(){
   let c1 = mouseX; let c2 = mouseY;
-  c1 = map(c1, 0, width, 0, 255)
+  c1 = map(c1, 0, width, 0, 255)     // Map the coordinates so it covers the entire canvas
   c2 = map(c2, 0, height, 0, 255);
 
-  if(keyIsDown(82)){
+  if(keyIsDown(82)){     // Turn ghost white when "r" is held down
     fill("white");
   }
-  else fill(c1, c2, 20);
+  else fill(c1, c2, 20); // Otherwise dependent on mouse location
 
 }
 
 
+
+
+// Draw character
 function charBot(){
   noStroke();
-  //if(keyIsDown(82)){
-  //  fill("white");
-  //}
-  //else fill(mouseX, mouseY, 20);
   botColor();
   circle(circleX, circleY, circleD);
   rect(circleX - circleD/2, circleY, circleD, 100);
-  triangle(circleX-50, circleY+100, circleX-25, circleY+100, circleX-37.5, circleY+133.3);
-  triangle(circleX-25, circleY+100, circleX, circleY+100, circleX-12.5, circleY+133.3);
-  triangle(circleX, circleY+100, circleX+25, circleY+100, circleX+12.5, circleY+133.3);
-  triangle(circleX+25, circleY+100, circleX+50, circleY+100, circleX+37.5, circleY+133.3);
+  triangle(circleX-50, circleY+99.5, circleX-25, circleY+99.5, circleX-37.5, circleY+133.3);
+  triangle(circleX-25, circleY+99.5, circleX, circleY+99.5, circleX-12.5, circleY+133.3);
+  triangle(circleX, circleY+99.5, circleX+25, circleY+99.5, circleX+12.5, circleY+133.3);
+  triangle(circleX+25, circleY+99.5, circleX+50, circleY+99.5, circleX+37.5, circleY+133.3);
   fill(0);
   circle(circleX - 27, circleY, 10);
   circle(circleX + 27, circleY, 10);
   stroke(0);
-  line(circleX - 20, circleY + 22, circleX + 20, circleY + 22);
-  
+  line(circleX - 20, circleY + 22, circleX + 20, circleY + 22); 
 }
 
+
+
+// Move bot with left and right arrows
 function moveBot(){
   if (keyIsDown(LEFT_ARROW)){
     circleX -= 10;
@@ -119,8 +173,10 @@ function moveBot(){
   }
 }
 
+
+
+// State variable to control background
 function currentBack(){
-  changeBack();
   switch(backNum){
     case 0:
       dayScene();
@@ -128,26 +184,33 @@ function currentBack(){
     case 1:
       nightScene();
       break;
+    case 2:
+      desert();
 
   }
 }
 
 
+// Call changeBack() when middle button pressed (returns 1)
+function click(event){
+  if(event.button == 1) {
+    changeBack();
+  }    
+  }
 
-function keyPressed(){
-  changeBack();
 
-}
+// Update backNum by 1 when called, wrap back to 0 if over 3
 function changeBack(){
   backNum += 1; 
-  if(backNum > 0){
+  if(backNum > 2){
     backNum = 0;
   }
 }
 
 
 function draw() {
-  currentBack();
+  atSea();
+  //currentBack();
   charBot();
   moveBot();
 }

c9f0199fe8f9e4b1a62cda9248a936174fbeedb6
MeixiY <ymeixi2008@gmail.com>
2026-03-30 12:22:09 -0600

    commit

--- Assignments/Assignment 3/sketch.js
+++ Assignments/Assignment 3/sketch.js
@@ -6,17 +6,28 @@
 // Globals
 let lineX;
 let vehicle;
+let eastbound = [];
+let westbound = [];
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   vehicle = new Vehicle();
+  for(let i = 0; i < 20; i++){
+    westbound.push(new Vehicle(0));
+  }
+  for(let i = 0; i < 20; i++){
+    eastbound.push(new Vehicle(0));
+  }
 }
 
 function draw() {
   background(220);
   road();
-  vehicle.action();
+  //vehicle.action();
+  for(let w of westbound){
+    w.action();
+  }
  
 }
 
@@ -31,12 +42,12 @@ function road(){
 }
 
 class Vehicle{
-  constructor(){
+  constructor(direction){
     this.type = Math.round(random(0,1));  
     this.color = color(random(255), random(255), random(255));
     this.x = 50;
     this.y = random(height/5 + 10, height-height*2/5 - 10);
-    this.d = Math.round(random(0, 1));
+    this.d = direction;
     this.xSpeed = random(5);
     this.timeShift = 0.01;
     this.time = random(100);
@@ -47,17 +58,17 @@ class Vehicle{
     noStroke();
     fill(this.color);
     if(this.type === 0){
-      rect(this.x, this.y, 70, 20);
+      rect(this.x, this.y, 60, 18);
       fill(255);
-      rect(this.x + 7, this.y + 21, 10, 3);
-      rect(this.x + 7, this.y - 3.5, 10, 3);
-      rect(this.x + 50, this.y + 21, 10, 3);
-      rect(this.x + 50, this.y - 3.5, 10, 3);
+      rect(this.x + 5, this.y + 19, 10, 3);
+      rect(this.x + 5, this.y - 3, 10, 3);
+      rect(this.x + 40, this.y + 19, 10, 3);
+      rect(this.x + 40, this.y - 3, 10, 3);
     }
 
     else if(this.type = 1){
-      rect(this.x, this.y, 50, 30);
-      rect(this.x + 52, this.y, 20, 30);
+      rect(this.x, this.y, 40, 25);
+      rect(this.x + 42, this.y, 15, 25);
     }
     fill(this.color);
   }
@@ -67,6 +78,10 @@ class Vehicle{
     if(this.x > width){
       this.x = 0;
     }
+    if(this.x < 0){
+      this.x = width;
+    }
+    
     if(this.xSpeed > 15){
       this.xSpeed = 15;
     }
@@ -94,6 +109,9 @@ class Vehicle{
   }
 
   action(){
+    //if(this.d = 1){
+      //rotate(180);
+    //}
     this.display();
     this.move();
     let choice = Math.round(random(100));
